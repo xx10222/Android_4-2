3cb4bed96b50edfa6e4d42573f5c78b932699173
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2021-12-21 12:07:01 +0900

    탭호스트 실습 java 코드

---
+++ 탭호스트/music.java
@@ -0,0 +1,54 @@
+package com.cookandroid.final_practice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.RadioButton;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import java.util.Date;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost=getTabHost();
+
+        TabHost.TabSpec tabSpecSong=tabHost.newTabSpec("SONG").setIndicator("음악별");
+        tabSpecSong.setContent(R.id.tabSong);
+        tabHost.addTab(tabSpecSong);
+
+        TabHost.TabSpec tabSpecArtist=tabHost.newTabSpec("Artist").setIndicator("가수별");
+        tabSpecArtist.setContent(R.id.tabArtist);
+        tabHost.addTab(tabSpecArtist);
+
+        TabHost.TabSpec tabSpecAlbum=tabHost.newTabSpec("Album").setIndicator("앨범별");
+        tabSpecAlbum.setContent(R.id.tabAlbum);
+        tabHost.addTab(tabSpecAlbum);
+
+        tabHost.setCurrentTab(0);
+    }
+}
